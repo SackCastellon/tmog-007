6857d5a864a4c6c2c964b5b11311361b18cd5f7a
Juan José González Abril <juanjo@gonzabril.es>
2024-03-12 09:08:31 +0100

    Implement solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,15 @@
 package org.tfoc;
 
+import java.util.Arrays;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
-
-    public boolean isAnagram(
-            String s,
-            String t) {
-
-        return false;
+    public boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) return false;
+        var arrayA = a.codePoints().sorted().toArray();
+        var arrayB = b.codePoints().sorted().toArray();
+        return Arrays.equals(arrayA, arrayB);
     }
-
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,53 @@
 package org.tfoc;
 
-import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-@Slf4j
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SolutionTest {
+    private Solution solution;
 
-    @Test
-    void testSolution() {
-
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
     }
 
+    @Test
+    void example1() {
+        var result = solution.isAnagram("anagram", "nagaram");
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void example2() {
+        var result = solution.isAnagram("rat", "car");
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void test() {
+        var random = new Random();
+        var codePoints = IntStream.generate(() -> random.nextInt(Character.MAX_CODE_POINT))
+            .limit(10_000)
+            .toArray();
+
+        var a = new String(codePoints, 0, codePoints.length);
+
+        for (int i = codePoints.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            // Simple swap
+            int tmp = codePoints[index];
+            codePoints[index] = codePoints[i];
+            codePoints[i] = tmp;
+        }
+
+        var b = new String(codePoints, 0, codePoints.length);
+
+        var result = solution.isAnagram(a, b);
+        assertThat(result).isTrue();
+    }
 }
